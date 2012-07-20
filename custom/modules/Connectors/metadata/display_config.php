0bd9d504163cc30c18c8e63131bc26ea30d94743
Swapnil Shinde <swapnil@bhea.co.in>
2012-07-20 09:41:33 +0530

    Commit Custom Directory For First Time By Swapnil Shinde on 20 july 2012

---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,32 @@
+<?php
+// created: 2012-07-18 13:37:32
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_soap_hoovers' => 'ext_soap_hoovers',
+    'ext_rest_twitter' => 'ext_rest_twitter',
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_rest_twitter' => 'ext_rest_twitter',
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_rest_twitter' => 'ext_rest_twitter',
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
