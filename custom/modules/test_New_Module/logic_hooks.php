bc42b7ac94fb099823ec19f24182dece58725b60
Swapnil Shinde <swapnil@bhea.co.in>
2012-07-23 19:09:19 +0530

    third commit

--- custom/modules/test_New_Module/logic_hooks.php
+++ custom/modules/test_New_Module/logic_hooks.php
@@ -9,7 +9,8 @@
 $hook_array = Array(); 
 // position, file, function 
 $hook_array['after_save'][] = Array(1, 'handleactivity', 'custom/modules/test_New_Module/HandleActivity.php','HandleActivity', 'handleactivities');
-$hook_array['after_save'][] = Array(1, 'handleactivity2', 'custom/modules/test_New_Module/HandleActivity2.php','HandleActivity2', 'handleactivities2');
+$hook_array['after_save'][] = Array(2, 'handleactivity2', 'custom/modules/test_New_Module/HandleActivity2.php','HandleActivity2', 'handleactivities2');
 
+$hook_array['after_save'][] = Array(3, 'handleactivity3', 'custom/modules/test_New_Module/HandleActivity3.php','HandleActivity3', 'handleactivities3');
 
 ?>
